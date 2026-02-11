310a0b63d60e5aca87ec5d480dbad980f386f013
Reijjo <teemu.aitomeri@gmail.com>
2026-02-11 18:51:46 +0200

    landing page features section updated

---
+++ client/src/app/_components/features/AddbetFeature.tsx
@@ -0,0 +1,37 @@
+import "./AddbetFeature.css";
+import Image from "next/image";
+
+export default function AddBetFeature() {
+  return (
+    <article className="add-bet-feature">
+      <div className="add-bet-feature-wrapper wrapper">
+        <div className="add-bet-image-container">
+          <p>
+            <strong>Add Bet</strong> has many different bet types to choose
+            from. <strong>Single</strong>, <strong>Over</strong>,{" "}
+            <strong>Under</strong>, <strong>Bet Builder</strong> and more!
+          </p>
+          <Image
+            src="/images/homepage/add1.png"
+            width={800}
+            height={450}
+            alt="add-bet-feature"
+          />
+        </div>
+        <div className="add-bet-image-container">
+          <p>
+            You can also add a <strong>Single Bet</strong> or a{" "}
+            <strong>Parlay</strong> and your favorite <strong>Tipper</strong>{" "}
+            and <strong>Sport/League</strong> that helps analyze the bets.
+          </p>
+          <Image
+            src="/images/homepage/add2.png"
+            width={800}
+            height={450}
+            alt="add-bet-feature"
+          />
+        </div>
+      </div>
+    </article>
+  );
+}
