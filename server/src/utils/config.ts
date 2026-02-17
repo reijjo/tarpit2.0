0b28e4bf472fb03b2ce711f4f396b3aa737e2d22
Reijjo <teemu.aitomeri@gmail.com>
2026-02-17 22:03:21 +0200

    fixing cicd

--- server/src/utils/config.ts
+++ server/src/utils/config.ts
@@ -5,9 +5,10 @@ const DB_URL: string = process.env.DB_URL || "";
 const DB_TEST_URL: string = process.env.DB_TEST_URL || "";
 
 const DATABASE_URL = isTest ? DB_TEST_URL : DB_URL;
+const requiredDbEnv = isTest ? "DB_TEST_URL" : "DB_URL";
 
 if (!DATABASE_URL) {
-  throw new Error("DB_URL environment variable is not set");
+  throw new Error(`${requiredDbEnv} environment variable is not set`);
 }
 
 export { PORT, isProduction, DATABASE_URL };
