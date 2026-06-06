3651ddcdf6db3b1873ec54a97c195ed3ffbcd955
Reijjo <teemu.aitomeri@gmail.com>
2026-06-06 17:41:06 +0300

    Betdetails form logic done, next the finishbet form

--- client/src/app/(app)/bets/add/page.tsx
+++ client/src/app/(app)/bets/add/page.tsx
@@ -1,17 +1,51 @@
 "use client";
 import "./layout.css";
-import { BetDetailsWithTempId } from "@/features/bets/types";
+import { INITIAL_BET_DETAILS } from "@/features/bets/constants";
+import { useBetFormDraft } from "@/features/bets/hooks";
+import {
+  BetDetailsFormValues,
+  BetDetailsWithTempId,
+} from "@/features/bets/types";
 import { useState } from "react";
 
 import BetDetailsForm from "@/features/bets/components/forms/BetDetailsForm";
+import FinishBetForm from "@/features/bets/components/forms/FinishBetForm";
 
 export default function AddPage() {
+  const [step, setStep] = useState<1 | 2>(1);
   const [betDetails, setBetDetails] = useState<BetDetailsWithTempId[]>([]);
+  const betDetailsFormDraft =
+    useBetFormDraft<BetDetailsFormValues>(INITIAL_BET_DETAILS);
+
+  const goBack = () => {
+    if (step === 2) setStep(1);
+  };
+
+  const editBet = (detail: BetDetailsWithTempId) => {
+    setBetDetails((prev) => prev.filter((d) => d.temp_id !== detail.temp_id));
+    betDetailsFormDraft.setDraft(detail);
+    goBack();
+  };
 
   return (
     <div className="add-page wrapper">
       <h1 className="like-h2 uppercase">Add bet</h1>
-      <BetDetailsForm betDetails={betDetails} setBetDetails={setBetDetails} />
+      {step === 1 ? (
+        <BetDetailsForm
+          betDetails={betDetails}
+          setBetDetails={setBetDetails}
+          setStep={setStep}
+          betDetailsFormDraft={betDetailsFormDraft}
+          onEdit={editBet}
+        />
+      ) : (
+        <FinishBetForm
+          betDetails={betDetails}
+          goBack={goBack}
+          setBetDetails={setBetDetails}
+          onEdit={editBet}
+        />
+      )}
     </div>
   );
 }
